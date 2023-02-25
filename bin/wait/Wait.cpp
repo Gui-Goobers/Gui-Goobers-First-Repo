8e46c1143ab345be1091a410880b61abd6418254
tristen <tristenpiston@hotmail.com>
2023-02-24 21:31:08 -0800

    Added to wait.cpp

---
+++ bin/wait/Wait.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "Wait.h"
+
+Wait::Wait(int argc, char **argv) : POSIXApplication(argc, argv)
+{
+
+}
+
+Wait::~Wait()
+{
+
+}
+
+Wait::Result Wait::exec()
+{
+    
+}
